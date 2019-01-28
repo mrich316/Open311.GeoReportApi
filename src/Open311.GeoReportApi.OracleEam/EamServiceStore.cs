e0afc5d4b6f0aa05e414182d0f1207f2be3a97b9
mrich316 <mrich316@gmail.com>
2019-01-28 14:12:16 -0500

    feat: hacky way to post service requests without adding a package in eam.

--- src/Open311.GeoReportApi.OracleEam/EamServiceStore.cs
+++ src/Open311.GeoReportApi.OracleEam/EamServiceStore.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Options;
 using Open311.GeoReportApi.InputModels;
 using Open311.GeoReportApi.Models;
 using Open311.GeoReportApi.Services;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Open311.GeoReportApi.OracleEam
 {
@@ -107,9 +108,85 @@ ORDER BY ffcu.column_seq_num
             }
         }
 
-        public Task<ServiceRequestCreated> Create(PostServiceRequestInputModel serviceRequest)
+        public async Task<ServiceRequestCreated> Create(PostServiceRequestInputModel serviceRequest)
         {
-            throw new System.NotImplementedException();
+            var service = await GetService(serviceRequest.ServiceCode, CancellationToken.None);
+            if (service == null) throw new ArgumentException($"ServiceCode {serviceRequest.ServiceCode} not found.");
+
+            using (var connection = await _options.CreateConnection())
+            {
+                try
+                {
+                    // TODO: The current version of Oracle.ManagedDataAccess.Core Version=2.18.3
+                    // does not support oracle objects. We cannot use WIP_EAM_WORK_REQUESTS%ROWTYPE
+                    // directly. We had to create an anonymous pl/sql block, but we can not return
+                    // any data from it.  Hence, ultra dirty hack, we use RAISE_APPLICATION_ERROR()
+                    // to return the work request id.
+                    await connection.ExecuteAsync($@"
+DECLARE
+  l_return_status    VARCHAR2(4000);
+  l_msg_count        NUMBER;
+  l_msg_data         VARCHAR2(4000); 
+  l_mode             VARCHAR2(15) := 'CREATE';
+  l_work_request_rec WIP_EAM_WORK_REQUESTS%ROWTYPE;
+  l_request_log      VARCHAR2(240) := :{nameof(serviceRequest.Description)};
+  l_work_request_id  NUMBER;
+BEGIN
+
+  l_work_request_rec.organization_id := :{nameof(serviceRequest.JurisdictionId)};
+  l_work_request_rec.work_request_type_id := :{nameof(serviceRequest.ServiceCode)};
+  l_work_request_rec.phone_number:= :{nameof(serviceRequest.Phone)};
+  l_work_request_rec.e_mail:= :{nameof(serviceRequest.Email)};
+  l_work_request_rec.work_request_priority_id := 2;
+  l_work_request_rec.work_request_status_id := 2;
+  l_work_request_rec.expected_resolution_date := SYSDATE;
+
+  WIP_EAM_WORKREQUEST_PUB.WORK_REQUEST_IMPORT(
+    p_api_version      => 1,
+    x_return_status    => l_return_status,
+    x_msg_count        => l_msg_count,
+    x_msg_data         => l_msg_data,
+    p_mode             => l_mode,
+    p_work_request_rec => l_work_request_rec,
+    p_request_log      => l_request_log,
+    p_user_id          => :{nameof(serviceRequest.AccountId)},
+    x_work_request_id  => l_work_request_id
+  );
+
+  IF (l_return_status = 'S')
+  THEN
+    RAISE_APPLICATION_ERROR(-20000, l_work_request_id);
+  ELSE  
+    RAISE_APPLICATION_ERROR(-20100, l_msg_data);
+  END IF;
+
+  DBMS_OUTPUT.PUT_LINE('l_work_request_id:' || l_work_request_id);
+  DBMS_OUTPUT.PUT_LINE('l_return_status: ' || l_return_status);
+  DBMS_OUTPUT.PUT_LINE('l_msg_count: ' || l_msg_count);
+  DBMS_OUTPUT.PUT_LINE('l_msg_data: ' || l_msg_data);
+
+END;
+",
+                        new
+                        {
+                            serviceRequest.AccountId,
+                            serviceRequest.Description,
+                            serviceRequest.JurisdictionId,
+                            serviceRequest.ServiceCode,
+                            serviceRequest.Phone,
+                            serviceRequest.Email
+                        });
+                }
+                catch (OracleException ex) when (ex.Number == 20000)
+                {
+                    // TODO: Yeah, pretty dirty and heavy hacking to simulate a function call.
+                    // See TODO from above SQL for the reason of this dirt.
+                    var serviceRequestId = ex.Message.Substring(11, ex.Message.IndexOf('\n') - 11);
+                    return new ServiceRequestCreated {ServiceRequestId = serviceRequestId};
+                }
+
+                throw new InvalidOperationException("This method always throws! Wait, what? Yeah. Hacky stuff.");
+            }
         }
 
         private async Task<ServiceAttribute> AddAttributeValues(ServiceAttribute attribute)
